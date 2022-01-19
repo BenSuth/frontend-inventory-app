771541dbac30be54fbfd16f18f18d1dd75f00309
BenSuth <sutherland681@gmail.com>
2022-01-18 20:06:13 -0500

    add item logic

---
+++ src/components/Item.js
@@ -0,0 +1,115 @@
+import React, {useState} from 'react'
+import { Form, Row, Button, Col} from 'react-bootstrap'
+import {useQuery, useMutation, gql} from '@apollo/client'
+import '../stylesheets/item.css'
+
+const UPDATE_ITEM_QUERY = gql`
+    mutation($itemId: ID!, $name: String, $description: String, $count: Int, $tags: [String!]) {
+        updateItem(input: {
+            itemId: $itemId
+            params:{
+                name: $name
+                description: $description
+                count: $count
+                tags: $tags
+            }
+            }) {
+            item {
+                id
+                name
+                description
+                count
+                tags
+            }
+        }
+    }
+`
+
+const DELETE_ITEM_QUERY = gql`
+    mutation($itemId: ID!) {
+        deleteItem(input: {itemId: $itemId}) {
+        item {
+            name
+            description
+            tags
+            count
+        }
+        }
+    }
+`
+
+const Item = ({item, refetch}) => {
+    const [updateItem, {loading: update_loading, error: update_error}] = useMutation(UPDATE_ITEM_QUERY)
+    const [deleteItem, {loading: delete_loading, error: delete_error}] = useMutation(DELETE_ITEM_QUERY)
+
+
+    const [name, setName] = useState(item.name)
+    const [description, setDescription] = useState(item.description)
+    const [count, setCount] = useState(item.count)
+    const [tags, setTags] = useState(item.tags)
+    const [disable, setDisable] = useState(true)
+
+    if (update_loading) return "Loading..."
+    if (update_error) return (<pre> {update_error.message} </pre>)  
+
+    if (delete_loading) return "Loading..."
+
+    const handleClick = () => {
+        setDisable(!disable)
+    }
+
+    const handleSubmit = async (evt) => {
+        evt.preventDefault();
+        await updateItem({variables: {itemId: item.id, name: name, description: description, count: count, tags: tags}})
+    }
+
+    const handleDelete = async (id) => {
+        await deleteItem({variables:{itemId: id}})
+        refetch()
+    }
+
+    return (
+        <div className="item-width">
+            <Form onSubmit={handleSubmit}>            
+                <fieldset disabled={disable}>
+                <Row>
+                    <Col>
+                        <Form.Control as="textarea" value={name} onChange={e => setName(e.target.value)}/>
+                    </Col>
+
+                    <Col>
+                        <Form.Control value={count} onChange={e => setCount(e.target.value)}/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Form.Group>
+                        <div>
+                            <Form.Control as="textarea" value={description} onChange={ e => setDescription(e.target.value)}/>
+                        </div>
+                    </Form.Group>
+                </Row>
+                <Row>
+                    <Form.Group as={Col}>
+                        <div>
+                            <Form.Control as="textarea" value={tags} onChange={e => setTags(e.target.value)}/>
+                        </div>
+                    </Form.Group>
+                </Row>
+                <Button type="submit">
+                    Update
+                </Button>
+                <legend>                
+                    <Button onClick={handleClick}>
+                        Edit
+                    </Button>
+                    <Button onClick={() => handleDelete(item.id)}> 
+                        Delete 
+                    </Button>
+                </legend>
+                </fieldset>
+            </Form>
+        </div>
+    )
+}
+
+export default Item;
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import Export from './Export'
+import Item from './Item'
 import {useQuery, useMutation, gql} from '@apollo/client'
 import { Form, Row, Button, Col, ListGroup, Card } from 'react-bootstrap'
 import '../stylesheets/items.css'
@@ -25,41 +26,6 @@ const CREATE_ITEM_QUERY = gql`
     }
 `
 
-const UPDATE_ITEM_QUERY = gql`
-    mutation($itemId: Int!, $name: String, $description: String, $count: Int, $tags: [String]) {
-        updateItem(input: {
-            itemId: $itemId
-            params:{
-                name: $name
-                description: $description
-                count: $count
-                tags: $tags
-            }
-            }) {
-            item {
-                id
-                name
-                description
-                count
-                tags
-            }
-        }
-    }
-`
-
-const DELETE_ITEM_QUERY = gql`
-    mutation($itemId: ID!) {
-        deleteItem(input: {itemId: $itemId}) {
-        item {
-            name
-            description
-            tags
-            count
-        }
-        }
-    }
-`
-
 const ITEMS_QUERY = gql`
     query {
         fetchItems {
@@ -74,55 +40,51 @@ const ITEMS_QUERY = gql`
 
 const Items = () => {
     const {data: item_data, loading: item_loading, error: item_error, refetch: refetch} = useQuery(ITEMS_QUERY)
-    const [createItem, {loading: create_loading, error: create_error}] = useMutation(CREATE_ITEM_QUERY)
-    const [updateItem, {loading: update_loading, error: update_error}] = useMutation(UPDATE_ITEM_QUERY)
-    const [deleteItem, {loading: delete_loading, error: delete_error}] = useMutation(DELETE_ITEM_QUERY)
+    const [createItem, {loading: create_loading, error: create_error}] = useMutation(CREATE_ITEM_QUERY, {errorPolicy: "none"})
 
     const [name, setName] = useState("")
     const [description, setDescription] = useState("")
     const [count, setCount] = useState(0)
     const [tags, setTags] = useState("")
-    const [isUpdate, setIsUpdate] = useState({})
+    const [error, setError] = useState("")
 
-    if (create_loading || update_loading || delete_loading) return "Loading..."
-    if (create_error) return (<pre> {create_error.message} </pre>)    
-    if (update_error) return (<pre> {update_error.message} </pre>)   
-    if (delete_error) return (<pre> {delete_error.message} </pre>)   
+    if (create_loading) return "Loading..."
 
     const handleSubmit = async (evt) => {
         evt.preventDefault();
-        if (parseInt(count) < 0) {
-            setCount(0)
-            return
-        }
 
-        await createItem({variables: {name: name, description: description, count: parseInt(count), tags: tags}})
-
+        createItem({variables: {name: name, description: description, count: parseInt(count), tags: tags}})
+            .then(({data}) => {
+                setError("")
+                refetch()
+            })
+            .catch(e => {
+                setError(e.toString())
+            })
         setName("")
         setDescription("")
         setCount(0)
         setTags("")
-        refetch()
-    }
-    
-    const handleDelete = async (id) => {
-        await deleteItem({variables:{itemId: id}})
-        refetch()
     }
 
     return (
         <div>
+            {error !== "" && <span> {error} </span>}
             <Form className="text" onSubmit={handleSubmit}>
                 <span> Create an Item! </span><br/>
                 <Row>
                     <Form.Group as={Col}>
                         <Form.Label>Name</Form.Label>
-                        <Form.Control value={name} onChange={e => setName(e.target.value)}/>
+                        <div>
+                            <Form.Control value={name} onChange={e => setName(e.target.value)}/>
+                        </div>
                     </Form.Group>
 
                     <Form.Group as={Col}>
                         <Form.Label>Count</Form.Label>
-                        <Form.Control value={count} onChange={e => setCount(e.target.value)}/>
+                        <div>
+                            <Form.Control value={count} onChange={e => setCount(e.target.value)}/>
+                        </div>
                     </Form.Group>
                 </Row>
                 <Row>
@@ -146,20 +108,12 @@ const Items = () => {
                 </Button>
             </Form>
             <div>
-            <h1> Items </h1>
+            <span> Items </span>
                 {!item_loading &&
                     <ListGroup variant="flush">
                         {item_data.fetchItems.map(item => (
                             <ListGroup.Item key={item.id}>
-                                    <Card>
-                                        <div className="card-text">
-                                            <Card.Header>{item.name}</Card.Header>
-                                            <Card.Title>{"Count: " + parseInt(item.count)}</Card.Title>
-                                            <Card.Body>{item.description} </Card.Body>
-                                            <Card.Footer>{item.tags} <br/></Card.Footer>
-                                            <button variant="primary" onClick={() => handleDelete(item.id)}>Delete</button>
-                                        </div>
-                                    </Card>
+                                <Item item={item} refetch={refetch}/>
                             </ListGroup.Item>
                         ))}
                     </ListGroup>
